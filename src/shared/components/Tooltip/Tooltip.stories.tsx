d2825526e9ac90f0d55670d36542e3bb6be81244
bytrustu <bytrustu@gmail.com>
2024-04-05 02:41:08 +0900

    feat: Tooltip 컴포넌트 추가

--- src/shared/components/Tooltip/Tooltip.stories.tsx
+++ src/shared/components/Tooltip/Tooltip.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { AppDisplay, Tooltip } from '@/shared';
+import { AppDisplay, Circle, styleToken, Tooltip, Typography } from '@/shared';
 
 const meta: Meta<typeof Tooltip> = {
   title: 'Primitive/Tooltip',
@@ -23,4 +23,43 @@ type Story = StoryObj<typeof Tooltip>;
 
 export const Primary: Story = {
   args: {},
+};
+
+export const WithBottom: Story = {
+  args: {
+    icon: (
+      <Circle backgroundColor="unset" border={`1px solid ${styleToken.color.gray400}`} cursor="pointer">
+        <Typography color={styleToken.color.gray400} variant="title" fontWeight={styleToken.fontWeight.bold}>
+          ?
+        </Typography>
+      </Circle>
+    ),
+    direction: 'bottom',
+  },
+};
+
+export const WithLeft: Story = {
+  args: {
+    icon: (
+      <Circle backgroundColor="unset" border={`1px solid ${styleToken.color.gray400}`} cursor="pointer">
+        <Typography color={styleToken.color.gray400} variant="title" fontWeight={styleToken.fontWeight.bold}>
+          ?
+        </Typography>
+      </Circle>
+    ),
+    direction: 'left',
+  },
+};
+
+export const WithRight: Story = {
+  args: {
+    icon: (
+      <Circle backgroundColor="unset" border={`1px solid ${styleToken.color.gray400}`} cursor="pointer">
+        <Typography color={styleToken.color.gray400} variant="title" fontWeight={styleToken.fontWeight.bold}>
+          ?
+        </Typography>
+      </Circle>
+    ),
+    direction: 'right',
+  },
 };
