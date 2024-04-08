6886c7766f2548b72c4eaded613042317a784301
bytrustu <bytrustu@gmail.com>
2024-04-09 02:56:33 +0900

    refactor: Box 타입확장을 위한 ComponentProps<'div'> 반영하도록 개선

--- src/shared/components/Box/Box.tsx
+++ src/shared/components/Box/Box.tsx
@@ -1,23 +1,15 @@
-import { PropsWithChildren, FocusEvent, forwardRef } from 'react';
+import { PropsWithChildren, forwardRef } from 'react';
+import type { ComponentProps } from 'react';
 import styled from '@emotion/styled';
 import { DefaultStyled } from '@/shared/styles';
 import type { AsProps, StyleProps } from '@/shared/types';
 
-type BoxProps = PropsWithChildren<
-  StyleProps &
-    AsProps & {
-      onBlur?: (e: FocusEvent<HTMLDivElement>) => void;
-      onClick?: () => void;
-      onMouseEnter?: () => void;
-      onMouseLeave?: () => void;
-      tabIndex?: number;
-    }
->;
+type BoxProps = PropsWithChildren<StyleProps & AsProps & Omit<ComponentProps<'div'>, 'ref'>>;
 
 const Root = styled(DefaultStyled)<BoxProps>``;
 
-export const Box = forwardRef<HTMLDivElement, BoxProps>(({ children, tabIndex = -1, ...props }, ref) => (
-  <Root tabIndex={tabIndex} ref={ref} {...props}>
+export const Box = forwardRef<HTMLDivElement, BoxProps>(({ children, ...props }, ref) => (
+  <Root ref={ref} {...props}>
     {children}
   </Root>
 ));
